9163eececc0cf33a8e6cede25cceecf8ff813a33
fch4ng <franciscochang@protonmail.com>
2022-11-06 16:08:54 -0300

    compania update

--- src/ej02/Compania.java
+++ src/ej02/Compania.java
@@ -1,5 +1,41 @@
 package ej02;
 
+import java.util.ArrayList;
+
 public class Compania {
+	
+	private String nombreC;
+	private ArrayList<Area> areas;
+	
+	public Compania(String nombreC, ArrayList<Area> areas) {
+		this.nombreC = nombreC;
+		this.areas = areas;
+	}
+	
+	public void listarAreas() {
+		
+		for (Area area : this.areas) {
+			
+			System.out.println(area.toString());
+		}
+	}
+
+	public String getNombreC() {
+		return nombreC;
+	}
+
+	public ArrayList<Area> getAreas() {
+		return areas;
+	}
+
+	private void setNombreC(String nombreC) {
+		this.nombreC = nombreC;
+	}
+
+	private void setAreas(ArrayList<Area> areas) {
+		this.areas = areas;
+	}
+	
+	
 
 }
